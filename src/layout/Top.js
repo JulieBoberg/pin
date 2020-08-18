8ccbf6e9f5125a1314d1be13e36967cc42afc655
JulieBoberg <43385336+JulieBoberg@users.noreply.github.com>
2020-08-18 16:58:31 -0700

    Trying out gestalt

---
+++ src/layout/Top.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Heading } from "gestalt";
+
+const Top = () => {
+  return (
+    <Box alignItems='center'>
+      <Heading>PIN PIN PIN</Heading>
+    </Box>
+  );
+};
+
+export default Top;
